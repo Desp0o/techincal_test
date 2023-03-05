95b27ad851bac8d3094bb0c419161ac34f79d685
Tornike Despotashvili <95018466+Desp0o@users.noreply.github.com>
2023-03-05 11:20:37 -0800

    added colors in tailwind

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@
 
 export default function App() {
   return (
-    <>
+    <div className="w-full h-[100vh] bg-[#1E1E1E] overflow-hidden">
     
-    </>
+    </div>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'xs': {'min':'0px', 'max':'768px'},
+        'xl': {'min':'1500px', 'max':''}
+      },
+
+      colors: {
+        'dark-navy': '#161819',
+        'lighter-navy': '#181A1B',
+        'dark-grey': '#252728',
+        'medium-grey': '#989898',
+        'accent-red': '#E40641',
+        'light-grey': '#EAECEF'
+      },
+    
+    },
+
+      
   },
   plugins: [],
 }
